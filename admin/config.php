a5c1eed771ebefdfc9d4c652f206a51428ddaacf
Robert <robert.bogach@gmail.com>
2014-07-05 13:41:21 +0300

    Start instal filter

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://niva21.end/admin/');
+define('HTTP_CATALOG', 'http://niva21.end/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://niva21.end/admin/');
+define('HTTPS_CATALOG', 'http://niva21.end/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/admin/');
+define('DIR_SYSTEM', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/system/');
+define('DIR_DATABASE', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/system/database/');
+define('DIR_LANGUAGE', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/admin/language/');
+define('DIR_TEMPLATE', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/admin/view/template/');
+define('DIR_CONFIG', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/system/config/');
+define('DIR_IMAGE', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/image/');
+define('DIR_CACHE', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/download/');
+define('DIR_LOGS', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/system/logs/');
+define('DIR_CATALOG', 'D:\Works_WWW\OpenServer\domains\localhost\niva21.end/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'db_niva21');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ admin/language/english/module/1000_categories_accordion.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']       = '1000 Categories Accordion Menu';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module 1000 Categories Accordion Menu!';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+
+// Entry
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Position:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Sort Order:';
+
+// Button
+$_['button_remove'] 	   = 'Remove';
+$_['button_add_module']    = 'Add';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module Category Accordion Menu!';
+?>
